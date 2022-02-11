9c7ac271c5dcdbd2871beb1f7a600e97f36fc9a4
Hui Chen <alex.chenhui@gmail.com>
2022-02-11 14:05:05 +0800

    hello

---
+++ basic/01.hello/build.sh
@@ -0,0 +1,10 @@
+#############################################################
+#   File Name: build.sh
+#     Autohor: Hui Chen (c) 2022
+#        Mail: alex.chenhui@gmail.com
+# Create Time: 2022/02/11-14:04:03
+#############################################################
+#!/bin/sh 
+go run hello.go 
+go build hello.go
+./hello
