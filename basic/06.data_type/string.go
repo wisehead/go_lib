834b158cb831e59f89eb297a604e8e66427202b3
Hui Chen <alex.chenhui@gmail.com>
2022-02-14 09:44:07 +0800

    06.data_type

---
+++ basic/06.data_type/string.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	str := "这里是 www\n.runoob\n.com"
+	fmt.Println("-------- 原字符串 ----------")
+	fmt.Println(str)
+	// 去除空格
+	str = strings.Replace(str, " ", "", -1)
+	// 去除换行符
+	str = strings.Replace(str, "\n", "", -1)
+	fmt.Println("-------- 去除空格与换行后 ----------")
+	fmt.Println(str)
+}
