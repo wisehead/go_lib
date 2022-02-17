707ff97a811ed0b757ccbbb08c5594ca275c7420
Hui Chen <alex.chenhui@gmail.com>
2022-02-17 20:14:18 +0800

    es2 sample

---
+++ elasticsearch/es2/build.sh
@@ -0,0 +1,9 @@
+#############################################################
+#   File Name: build.sh
+#     Autohor: Hui Chen (c) 2022
+#        Mail: alex.chenhui@gmail.com
+# Create Time: 2022/02/17-19:24:22
+#############################################################
+#!/bin/sh 
+go mod init es2
+go mod tidy
